7a7f70fff3aa984d7b919cf20b66dad9d4f15bcc
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-26 14:28:48 -0700

    Part 1 >>>OPERATIONAL
    >>>MODIFICATIONS
    --InfiniteSeries.java
      -BUILT check method
      -COMMENT accuracy of myexp method
    
    >>>DEFICITS
    --
    
    >>>BUGS
    --
    
    >>>THROWS
    --

--- src/InfiniteSeries.java
+++ src/InfiniteSeries.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class InfiniteSeries {
 
-    private static double result = 1;
+    private static double expVal = 1;
     private static int exponent;
     private static int iterations;
     private static final Scanner s = new Scanner(System.in);
+    private static final String template = "%-10s %-20s%-20s%n";
 
     /**
      * Prompts user for int value
@@ -25,13 +26,13 @@ public class InfiniteSeries {
      * @param n: number summation terms
      */
     private static void myexp(int x, int n) {
-        double expVal = 1;
+        double result = 1;
         if (x == 0 && n == 0) {
             System.out.println("Knuth and Benson are coming for you.");
         } else if (x != 0 && n == 0) {
             System.out.println(expVal);
         } else {
-            for (int i = 1; i <= n; i++){
+            for (int i = 1; i <= n; i++){       //Accuracy maxes out at 17 iterations.
                 result *= x;
                 result /= i;
                 expVal += result;
@@ -40,14 +41,23 @@ public class InfiniteSeries {
         }
     }
 
-
+    /**
+     * Prints the exponent, and compares the method-calculated value of e^x with the package value of e^x
+     * @author Atticus Blanco
+     * @param exponent: instance variable
+     */
+    private static void check(int exponent) {
+        System.out.printf(template, "Exponent", "Calculated Value", "Package Value");
+        System.out.printf(template , exponent, expVal, Math.exp(exponent));
+    }
 
     /**
-     * Calls getUserVals method, then passes resulting instance values to myexp method
+     * Calls getUserVals method, Calls myexp method and passes instance variables exponent and iterations, calles check method and passes instance variable exponent
      * @author Atticus Blanco
      */
     public static void main (String[] args){
         getUserVals();
         myexp(exponent, iterations);
+        check(exponent);
     }
 }
